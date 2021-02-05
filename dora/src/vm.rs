core-lang/core

Add annotations to KnownElements

dora/src/vm.rs
@@ -21,13 +21,13 @@ use crate::sym::{SymTable, TermSym};
 use crate::threads::{Threads, STACK_SIZE, THREAD};
 use crate::ty::{BuiltinType, LambdaTypes, TypeList, TypeLists};
 use crate::utils::GrowableVec;
-use crate::vm::module::{Module, ModuleDef, ModuleId};
 
 use dora_parser::ast;
 use dora_parser::interner::*;
 use dora_parser::lexer::File;
 use dora_parser::parser::NodeIdGenerator;
 
+pub use self::annotation::{Annotation, AnnotationId};
 pub use self::class::{
     find_field_in_class, find_method_in_class, find_methods_in_class, Class, ClassDef, ClassDefId,
     ClassId, TypeParam,
@@ -41,7 +41,10 @@ pub use self::fct::{Fct, FctId, FctKind, FctParent, Intrinsic};
 pub use self::field::{Field, FieldDef, FieldId};
 pub use self::global::{GlobalData, GlobalId};
 pub use self::impls::{ImplData, ImplId};
-pub use self::known::{KnownClasses, KnownElements, KnownFunctions, KnownModules, KnownTraits};
+pub use self::known::{
+    KnownAnnotations, KnownClasses, KnownElements, KnownFunctions, KnownModules, KnownTraits,
+};
+pub use self::module::{Module, ModuleDef, ModuleId};
 pub use self::src::{CallType, ConvInfo, FctSrc, ForTypeInfo, IdentType, NodeMap, Var, VarId};
 pub use self::strct::{
     StructData, StructDef, StructDefId, StructFieldData, StructFieldDef, StructId,
@@ -49,6 +52,7 @@ pub use self::strct::{
 pub use self::traits::{TraitData, TraitId};
 pub use self::tuple::{ensure_tuple, TupleId, Tuples};
 
+pub mod annotation;
 pub mod class;
 mod cnst;
 mod enums;
@@ -102,6 +106,7 @@ pub struct VM<'ast> {
     pub tuples: Mutex<Tuples>,                 // stores all tuple definitions
     pub modules: GrowableVec<RwLock<Module>>,  // stores all module source definitions
     pub module_defs: GrowableVec<RwLock<ModuleDef>>, // stores all module definitions
+    pub annotations: GrowableVec<RwLock<Annotation>>, // stores all annotation source definitions
     pub fcts: GrowableVec<RwLock<Fct<'ast>>>,  // stores all function source definitions
     pub jit_fcts: GrowableVec<JitFct>,         // stores all function implementations
     pub enums: Vec<RwLock<EnumData>>,          // store all enum source definitions
@@ -128,6 +133,7 @@ impl<'ast> VM<'ast> {
         let empty_module_id: ModuleId = 0.into();
         let empty_trait_id: TraitId = 0.into();
         let empty_fct_id: FctId = 0.into();
+        let empty_annotation_id: AnnotationId = 0.into();
         let gc = Gc::new(&args);
 
         let vm = Box::new(VM {
@@ -142,6 +148,7 @@ impl<'ast> VM<'ast> {
             tuples: Mutex::new(Tuples::new()),
             modules: GrowableVec::new(),
             module_defs: GrowableVec::new(),
+            annotations: GrowableVec::new(),
             enums: Vec::new(),
             enum_defs: GrowableVec::new(),
             traits: Vec::new(),
@@ -173,18 +180,33 @@ impl<'ast> VM<'ast> {
                     string_buffer: empty_module_id,
                 },
 
-                functions: KnownFunctions {
-                    string_buffer_empty: empty_fct_id,
-                    string_buffer_append: empty_fct_id,
-                    string_buffer_to_string: empty_fct_id,
-                },
-
                 traits: KnownTraits {
                     equals: empty_trait_id,
                     comparable: empty_trait_id,
                     stringable: empty_trait_id,
                     iterator: empty_trait_id,
                     zero: empty_trait_id,
+                },
+
+                annotations: KnownAnnotations {
+                    abstract_: empty_annotation_id,
+                    final_: empty_annotation_id,
+                    internal: empty_annotation_id,
+                    override_: empty_annotation_id,
+                    open: empty_annotation_id,
+                    pub_: empty_annotation_id,
+                    static_: empty_annotation_id,
+
+                    test: empty_annotation_id,
+
+                    cannon: empty_annotation_id,
+                    optimize_immediately: empty_annotation_id,
+                },
+
+                functions: KnownFunctions {
+                    string_buffer_empty: empty_fct_id,
+                    string_buffer_append: empty_fct_id,
+                    string_buffer_to_string: empty_fct_id,
                 },
 
                 byte_array_def: Mutex::new(None),
@@ -312,6 +334,12 @@ impl<'ast> VM<'ast> {
     pub fn enum_by_name(&self, name: &'static str) -> EnumId {
         let name = self.interner.intern(name);
         self.sym.lock().get_enum(name).expect("class not found")
+    }
+
+    #[cfg(test)]
+    pub fn module_by_name(&self, name: &'static str) -> ModuleId {
+        let name = self.interner.intern(name);
+        self.sym.lock().get_module(name).expect("module not found")
     }
 
     #[cfg(test)]

soc/dora

dora/src/vm.rs
@@ -26,6 +26,7 @@ use dora_parser::ast;
 use dora_parser::interner::*;
 use dora_parser::parser::NodeIdGenerator;
 
+pub use self::annotations::{Annotation, AnnotationId};
 pub use self::classes::{
     class_accessible_from, class_field_accessible_from, find_field_in_class, find_method_in_class,
     find_methods_in_class, method_accessible_from, Candidate, Class, ClassDef, ClassDefId, ClassId,
@@ -42,7 +43,8 @@ pub use self::globals::{global_accessible_from, init_global_addresses, GlobalDat
 pub use self::impls::{find_trait_impl, impl_matches, ImplData, ImplId};
 pub use self::imports::ImportData;
 pub use self::known::{
-    KnownClasses, KnownElements, KnownEnums, KnownFunctions, KnownStructs, KnownTraits,
+    KnownAnnotations, KnownClasses, KnownElements, KnownEnums, KnownFunctions, KnownStructs,
+    KnownTraits,
 };
 pub use self::modules::{
     find_methods_in_module, module_accessible_from, Module, ModuleDef, ModuleDefId, ModuleId,
@@ -121,6 +123,7 @@ pub struct VM {
     pub tuples: Mutex<Tuples>,                  // stores all tuple definitions
     pub modules: GrowableVec<RwLock<Module>>,   // stores all module source definitions
     pub module_defs: GrowableVec<RwLock<ModuleDef>>, // stores all module definitions
+    pub annotations: GrowableVec<RwLock<Annotation>>, // stores all annotation source definitions
     pub namespaces: Vec<NamespaceData>,         // storer all namespace definitions
     pub fcts: GrowableVec<RwLock<Fct>>,         // stores all function source definitions
     pub jit_fcts: GrowableVec<JitFct>,          // stores all function implementations
@@ -154,6 +157,7 @@ impl VM {
         let empty_fct_id: FctId = 0.into();
         let empty_enum_id: EnumId = 0.into();
         let empty_struct_id = 0.into();
+        let empty_annotation_id: AnnotationId = 0.into();
         let gc = Gc::new(&args);
 
         let prelude_namespace_id = NamespaceId(0);
@@ -184,6 +188,7 @@ impl VM {
             tuples: Mutex::new(Tuples::new()),
             modules: GrowableVec::new(),
             module_defs: GrowableVec::new(),
+            annotations: GrowableVec::new(),
             namespaces,
             enums: Vec::new(),
             enum_defs: GrowableVec::new(),
@@ -211,6 +216,21 @@ impl VM {
 
                 enums: KnownEnums {
                     option: empty_enum_id,
+                },
+
+                annotations: KnownAnnotations {
+                    abstract_: empty_annotation_id,
+                    final_: empty_annotation_id,
+                    internal: empty_annotation_id,
+                    override_: empty_annotation_id,
+                    open: empty_annotation_id,
+                    pub_: empty_annotation_id,
+                    static_: empty_annotation_id,
+
+                    test: empty_annotation_id,
+
+                    cannon: empty_annotation_id,
+                    optimize_immediately: empty_annotation_id,
                 },
 
                 structs: KnownStructs {